c1e1561fd2f9a455a7d957291207400f0d5a8efe
Your Name <you@example.com>
2024-06-20 18:22:00 +0530

    Send mail

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require("./models/router"); // import the router
 const cors = require('cors');
+require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 const app = express();
--- models/router.js
+++ models/router.js
@@ -1,9 +1,49 @@
 const express = require('express');
 const router = new express.Router();
+const nodemailer = require("nodemailer");
 
 router.post("/register",(req,res)=>{
     console.log(req.body)
+    // const {email, message, phone, name} = req.body;
+    const { email, message, phone, name } = req.body;
+
+    try {
+        const transporter = nodemailer.createTransport({
+            service:"gmail",
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD,
+              },
+        })
+
+        const mailOptions = {
+            from: process.env.EMAIL,    
+            to: email,
+            subject: "Registration Details",
+            html: `
+              <b>Congratulation ${name}, you have successfully registered!</b>
+              <p>Your registration details are as follows:</p>
+              <p>username: ${name}</p>
+              <p>Email: ${email}</p>
+              <p>Phone Number: ${phone}</p>
+              <p>Message: ${message}</p>
+            `,
+          }
+
+        transporter.sendMail(mailOptions,(error,info)=>{
+            if(error){
+                console.log("Error",error)
+                res.status(500).json({status:500,message:"Failed to send email"})
+            }else{
+                console.log("Email Sent" + info.response);
+                res.status(201).json({status:201,message:"Email sent successfully"})
+            }
+        })
+    } catch (error) {
+        console.log("Error",error)
+        res.status(500).json({status:500,message:"Internal Server Error"})
+    }
 
 })
 
-module.exports = router;
+module.exports = router
