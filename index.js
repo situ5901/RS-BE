fe3b0b658563365caac4ecccba99aa69601e3659
Situ-Kumar <er.situkumar@gmail.com>
2024-10-05 00:51:05 +0530

    BE uptodate

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require("./models/router"); // import the router
 const cors = require('cors');
+const path = require('path');
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 const userModel = require('./models/students');
 const popModel = require('./models/popups');
@@ -14,6 +14,7 @@ app.use(cors()); // enable CORS
 app.use(router); // use the router
 
 app.get('/data', async (req, res) => {
+  console.log("Feath data.....");
   try {
     const users = await userModel.find().exec();
     res.json(users);
@@ -23,7 +24,8 @@ app.get('/data', async (req, res) => {
   }
 });
 
-app.get('/situ', async (req, res) => {
+app.get('/popup', async (req, res) => {
+  console.log("Feath Popup.....");
   try {
     const pops = await popModel.find().exec();
     res.json(pops);
@@ -34,7 +36,8 @@ app.get('/situ', async (req, res) => {
 });
 
 // Updated MongoDB connection string
-mongoose.connect("mongodb+srv://RS-Tech:er.situkumar.com@cluster0.icf0z.mongodb.net/")
+mongoose.connect("mongodb+srv://RS-Tech:er.situkumar.com@cluster0.icf0z.mongodb.net/RS-Tech?retryWrites=true&w=majority")
+
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
 
